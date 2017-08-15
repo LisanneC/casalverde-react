1e0e829720c6af79b2165b404c16bf4461e9aa7f
lisanne <lisanne.chin@gmail.com>
2017-08-15 10:15:18 +0200

    Create PagesContainer and test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import { shallow } from 'enzyme'
+import chai, { expect } from 'chai'
+import chaiEnzyme from 'chai-enzyme'
+import App from './App'
+import PagesContainer from './pages/PagesContainer'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+chai.use(chaiEnzyme())
+
+describe('<App />', () => {
+  const app = shallow(<App />)
+
+  it('wraps everything in a div tag', () => {
+    expect(app).to.have.tagName('div')
+  })
+
+  it('contains the PagesContainer', () => {
+    expect(app).to.have.descendants(PagesContainer)
+  })
+})
