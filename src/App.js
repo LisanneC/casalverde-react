1970f62032281f1c19faff13a6b4fa87fe17a4bd
Mojganx <mojgan.zolghadr@gmail.com>
2017-08-15 14:36:03 +0200

    added text field and buttons

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Title from './components/Title'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import muiTheme from './assets/styles/themes'
 import PagesContainer from './pages/PagesContainer'
+import Comment from './pages/Comment'
 
-class App extends Component {
+const pages = [
+  {
+    title: 'hoi',
+    text: 'even kijken of dit werkt'
+  },
+]
+
+
+class App extends PureComponent {
   render() {
     return (
     <MuiThemeProvider muiTheme={muiTheme}>
       <div>
-         <Title content="Hello Admin"/>
-         <Title content="What would you like to edit today?"/>
-         <PagesContainer pages={ [] } />
+
+         <PagesContainer pages={ pages } />
+         <Comment> <input type="text" ref="Paragraph" placeholder="Paragraph 1"/> </Comment>
+         <Comment> <input type="text" ref="Paragraph" placeholder="Paragraph 2"/> </Comment>
+         <Comment> <input type="text" ref="Paragraph" placeholder="Paragraph 3"/> </Comment>
       </div>
     </MuiThemeProvider>
     );
