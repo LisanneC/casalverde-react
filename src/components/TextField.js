80f78e0c5801cc1cf58270a3532d717f4176ac08
lisanne <lisanne.chin@gmail.com>
2017-08-16 14:51:25 +0200

    dont merge this

--- src/components/TextField.js
+++ src/components/TextField.js
@@ -3,20 +3,55 @@ import TextField from 'material-ui/TextField';
 import FlatButton from 'material-ui/FlatButton';
 
 
-const TextFieldFloat = () => (
-  <div>
-		<TextField
-      hintText="Add a Title"
-      floatingLabelText="Title" /> <br/>
-    <TextField
-      hintText="Add a Remark"
-      floatingLabelText="Remarks" /> <br/>
-     <FlatButton label="Save" secondary={true} />
-     <FlatButton label="Delete" primary={true} />
+let TextFieldFloat = React.createClass({
+  getInitialState: function() {
+    return {editing: false}
+  },
+  edit: function () {
+      this.setState({editing: true});
+      console.log('test')
+      console.log(this.state)
+  },
+  delete: function () {
+    console.log("Removing");
+  },
+  save: function () {
+      this.setState({editing: false});
+      // console.log(this.state)
+  },
 
-  </div>
+  renderNormal: function () {
+    return (
+      <div className="textContainer">
 
+        <TextField hintText="Add a Title" floatingLabelText="Title" /> <br/>
+        <TextField hintText="Message Field" floatingLabelText="MultiLine and FloatingLabel"
+            multiLine={true} rows={2} /> <br/>
+        <FlatButton onClick={this.edit} label="Edit" secondary={true} />
+        <FlatButton onClick={this.delete} label="Delete" primary={true} />
+      </div>
+   );
+  },
 
-);
+  renderForm: function () {
+    return (
+      <div className="commentContainer">
+        <TextField hintText="Add a Title" floatingLabelText="Title" /> <br/>
+        <TextField hintText="Message Field" floatingLabelText="MultiLine and FloatingLabel"
+            multiLine={true} rows={2} /> <br/>
+        <FlatButton onClick={this.save} label="Save" secondary={true} />
+      </div>
+   );
+  },
+
+  render:function () {
+    // console.log(this.state.editing)
+    if(this.state.editing) {
+      return this.renderForm();
+    }else{
+      return this.renderNormal();
+    }
+  }
+})
 
 export default TextFieldFloat;
