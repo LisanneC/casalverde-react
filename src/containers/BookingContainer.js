63ef1b530194cca3f7623cfeb12d7768bb5dfb13
lisanne <lisanne.chin@gmail.com>
2017-08-19 19:39:47 +0200

    Error Unhandled Rejection

--- src/containers/BookingContainer.js
+++ src/containers/BookingContainer.js
@@ -1,52 +1,72 @@
-import React, { PureComponent } from 'react';
-import Title from '../components/Title';
-import {connect} from 'react-redux';
-import HomePage from '../components/HomePage';
-import RaisedButton from 'material-ui/RaisedButton';
+// import React, { PureComponent } from 'react';
+// import Title from '../components/Title';
+// import {connect} from 'react-redux';
+// import HomePage from '../components/HomePage';
+// import RaisedButton from 'material-ui/RaisedButton';
 import BookingPage from '../components/BookingPage';
-import ImageUploader from '../components/ImageUploader';
+// import ImageUploader from '../components/ImageUploader';
 import { fetchBookings } from '../actions/bookingActions';
-import LoadingComponent from './LoadingComponent';
+// import LoadingComponent from './LoadingComponent';
 
+
+import React, { PureComponent } from 'react';
+import {connect} from 'react-redux';
+import ImageUploader from '../components/ImageUploader';
+import LoadingComponent from './LoadingComponent';
+import BookingContent from '../components/BookingContent';
 
 class BookingContainer extends LoadingComponent {
+
   constructor(props){
     super(props);
     this.state = {
-      edit: false,
-      buttonName: 'Edit',
+      content: [],
     }
   }
 
-componentWillMount() {
-  this.props.dispatch(fetchBookings())
-}
+  componentWillMount() {
+    this.loadBookingContent();
+  }
+
+  renderContent() {
+    return this.state.content.map(item =>  {
+      return (
+        <BookingContent key={item.id}
+          handleOnSave={this.saveBookingContent.bind(this)}
+          handleOnUpdate={this.updateBookingContent.bind(this)}
+          handleOnDelete={this.deleteParagraph.bind(this)}
+          {...item}
+        />
+      )
+    });
+  }
 
   render() {
     return(
       <div className="Pages wrapper">
-        <main>
+        <div>
           <ImageUploader />
-          <BookingPage edit={this.state.edit} content={this.props.bookingPageContent} />
-          <RaisedButton label={this.state.buttonName} primary={true} onClick={this.handleOnClick.bind(this)} />
-        </main>
+          {this.renderContent()}
+        </div>
       </div>
     )
   }
 
-  handleOnClick() {
-    if(this.state.edit){
-      this.setState({
-        edit: false,
-        buttonName: 'Edit'
-      });
-    }else {
-      this.setState({
-        edit: true,
-        buttonName: 'Save'
-      });
+  loadBookingContent() {
+    let newContent = {heading: 'Add New Header', text: 'Add New Text', new: true};
+    this.loadContent('bookings', 'GET', newContent);
+  }
 
-    }
+  saveBookingContent(item) {
+    this.saveContent('bookings', 'POST', item);
+  }
+
+  updateBookingContent(item) {
+    this.updateContent(`bookings/${item.id}`, 'PUT', item);
+  }
+
+  deleteParagraph(id){
+    this.deleteContent(`bookings/${id}`, 'DELETE');
   }
 }
 
