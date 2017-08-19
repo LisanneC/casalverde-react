f0d0e4c9e96430fd8163a7b9a33398ced80d9e21
DKateryna <derkach.kateryna@gmail.com>
2017-08-19 15:28:14 +0200

    Added reject booking functionality

--- src/components/Confirmed.js
+++ src/components/Confirmed.js
@@ -4,15 +4,25 @@ import { saveBooking } from '../actions/bookingActions';
 
 
 class Confirmed extends React.Component {
-  onClick (){
+  confirm (){
     let booking = {...this.props.booking, confirmed: true};
     this.props.dispatch(saveBooking(booking));
   }
+  reject (){
+    let booking = {...this.props.booking, confirmed: false};
+    this.props.dispatch(saveBooking(booking));
+  }
+
+
 
   render () {
     return (
       <div>
-        <button onClick={this.onClick.bind(this)}>Confirm</button>
+        {this.props.booking.confirmed ? (
+          <button onClick={this.reject.bind(this)}>Reject</button>
+        ) : (
+          <button onClick={this.confirm.bind(this)}>Confirm</button>
+        )}
       </div>)
     }
   }
