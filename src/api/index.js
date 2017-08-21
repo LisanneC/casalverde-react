72225a82b210c83f85ad9e9ddff0ee48ccfc6bbd
smilingkite <mail@katinkahesselink.net>
2017-08-21 13:35:29 +0200

    React deploy attempt#1

--- src/api/index.js
+++ src/api/index.js
@@ -12,62 +12,63 @@
 //   .then((response) => ( do smth with response.body...))
 //   .catch((error) => (...))
 
-import request from 'superagent'
+import request from 'superagent';
 
-const RAILS_HOST = 'http://localhost:5000/admin' // <-- note: no trailing slash! :)
+const RAILS_HOST =
+	process.env !== 'production'
+		? 'http://localhost:5000/admin'
+		: 'https://casalverdeitaly.herokuapp.com/admin'; // <-- note: no trailing slash! :)
 
 export default class API {
-  resourcePath(resource) {
-    return [RAILS_HOST, resource].join('/')
-  }
+	resourcePath(resource) {
+		return [RAILS_HOST, resource].join('/');
+	}
 
-  // GET request /resource
-  //
-  // Example: api.find('users')
-  //            .then((response) => ( do smth with response.body...))
-  //            .catch((error) => (...))
-  //
-  // Returns: a Promise
-  find(resource) {
-    return request
-      .get(this.resourcePath(resource))
-      .withCredentials()
-      .type('json')
-      .accept('json')
-  }
+	// GET request /resource
+	//
+	// Example: api.find('users')
+	//            .then((response) => ( do smth with response.body...))
+	//            .catch((error) => (...))
+	//
+	// Returns: a Promise
+	find(resource) {
+		return request
+			.get(this.resourcePath(resource))
+			.withCredentials()
+			.type('json')
+			.accept('json');
+	}
 
-  // POST request /resource
-  //
-  // Example: api.create('users', { email: 'foo@bar.co' })
-  //            .then((response) => ( do smth with response.body...))
-  //            .catch((error) => (...))
-  //
-  // Returns: a Promise
-  create(resource, data) {
-    return request
-      .post(this.resourcePath(resource))
-      .send(data)
-      .withCredentials()
-      .type('json')
-      .accept('json')
-  }
+	// POST request /resource
+	//
+	// Example: api.create('users', { email: 'foo@bar.co' })
+	//            .then((response) => ( do smth with response.body...))
+	//            .catch((error) => (...))
+	//
+	// Returns: a Promise
+	create(resource, data) {
+		return request
+			.post(this.resourcePath(resource))
+			.send(data)
+			.withCredentials()
+			.type('json')
+			.accept('json');
+	}
 
+	update(resource, id, data) {
+		return request
+			.patch(this.resourcePath(resource) + `/${id}`)
+			.send(data)
+			.withCredentials()
+			.type('json')
+			.accept('json');
+	}
 
-  update(resource, id, data) {
-    return request
-      .patch(this.resourcePath(resource) +`/${id}`)
-      .send(data)
-      .withCredentials()
-      .type('json')
-      .accept('json')
-  }
-
-  destroy(resource, id) {
-    return request
-      .delete(this.resourcePath(resource) +`/${id}`)
-      .withCredentials()
-      .type('json')
-      .accept('json')
-  }
-
+	destroy(resource, id) {
+		return request
+			.delete(this.resourcePath(resource) + `/${id}`)
+			.withCredentials()
+			.type('json')
+			.accept('json');
+	}
 }
